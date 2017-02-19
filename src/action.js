c8b1179988b22d06e594c9ea0d78f629dffe89c1
Ziv <zuoww@ychcaptial.com>
2017-02-19 17:34:44 +0800

    bug fixed

--- src/action.js
+++ src/action.js
@@ -4,15 +4,15 @@ var chalk = require('chalk');
 module.exports = {
 
     runPath: function(path, options) {
-        var dir =  path ? `${process.env.PWD}/${path}` : process.env.PWD;
+        var currentWorkPath = process.cwd();
+        var dir =  path ? path : currentWorkPath;
         server.runPath(dir, options.port)
     },
 
     runConfig: function(path) {
-        path = path || `${process.env.PWD}/config.cup.json`
+        path = path || `${process.cwd()}/config.cup.json`
         var cupConfig = require(path)
         console.log('Current app is running by your custom cofig:\n\n', chalk.blue(`${path} \n`))
-        // console.log(chalk.blue(`${path} \n`))
         server.runConfig(cupConfig)
     },
 }
