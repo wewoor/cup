65ae2e2697586126ee6cc28d225d0fd2c60a2bdf
xiaowei <wewoor@gmail.com>
2022-06-16 11:10:07 +0800

    feat: simplify the Run command logic

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,6 @@ process.title = 'cup'
 const program = require('commander')
 
 const packageInfo = require('../package.json')
-const logger = require('../src/logger').default
 const { runByPath, runByConfig } = require('../src/actions').default
 
 try {
@@ -15,26 +14,26 @@ try {
         .version(packageInfo.version)
 
     program
-        .command('serve')
-        .description('startup the server and serve the path you specified.')
+        .command('run')
+        .description('startup the Express server by the directory or cup.config.js you specified.')
         .arguments('[path]', 'required the root path of the server.')
         .option('-p, --port <port>', 'use custom server port. default is 3000.')
         .option('-c, --config', 'use cup.config.js or cup.config.json to startup the server.')
         .action((path, options) => {
             if (path) {
                 runByPath(path, options)
-            } else {
+            } else if (options.config) {
                 runByConfig(path)
             }
         })
 
     program.on('--help', function () {
-        logger.info('\nExamples:')
-        logger.info('$ cup --help')
-        logger.info('$ cup -h')
+        console.log('\nExamples:')
+        console.log('  $ cup run --help')
+        console.log('  $ cup run -h')
     })
 
     program.parse(process.argv)
 } catch (e) {
-    logger.error('Exec error.' + e)
+    console.error('Exec error.' + e)
 }
